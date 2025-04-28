d50b299c641b3cff0acddfcc4edb2585df24adf3
Max <ibroden@yandex.ru>
2025-04-28 22:11:24 +0300

    [linting] Satisfy flake8.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.routers import books, favourites, reading_list
+from .routers import books, favourites, reading_list
 
 app = FastAPI(
     title="Book Tracking API",
@@ -7,6 +7,12 @@ app = FastAPI(
     version="1.0.0"
 )
 
-app.include_router(favourites.router, prefix="/favourites", tags=["favourites"])
-app.include_router(reading_list.router, prefix="/reading-list", tags=["reading-list"])
-app.include_router(books.router, prefix="/books", tags=["books"])
+app.include_router(favourites.router,
+                   prefix="/favourites",
+                   tags=["favourites"])
+app.include_router(reading_list.router,
+                   prefix="/reading-list",
+                   tags=["reading-list"])
+app.include_router(books.router,
+                   prefix="/books",
+                   tags=["books"])
