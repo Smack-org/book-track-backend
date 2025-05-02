0a96856385aa01452be9954b57c6f0da69f61166
Max <ibroden@yandex.ru>
2025-05-02 11:22:10 +0300

    prettify tests.

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,29 +1,4 @@
-import httpx
-
-
-class API:
-    def __init__(self, base_url="http://api:8000"):
-        self.client = httpx.Client(base_url=base_url)
-
-    def register_user(self, login: str, password: str, username: str):
-        return self.client.post("/users/new", json={
-            "login": login,
-            "password": password,
-            "username": username,
-        })
-
-    def login(self, username: str, password: str):
-        return self.client.post("/users/token", data={
-            "username": username,
-            "password": password,
-        })
-
-    def get_profile(self, token: str):
-        headers = {"Authorization": f"Bearer {token}"}
-        return self.client.get("/users/me", headers=headers)
-
-    def close(self):
-        self.client.close()
+from .conftest import API
 
 
 def test_user_flow_against_running_api():
