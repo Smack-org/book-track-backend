60b24b4cd8cdc5b722357096a70301085300825c
Max <ibroden@yandex.ru>
2025-05-02 11:05:35 +0300

    Fail if coverage < 60%

--- run_e2e.sh
+++ run_e2e.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker compose --file compose.test.yaml up --build -d > /dev/null
+docker compose --file compose.test.yaml up --build -d
 
 # grant the tests 10 seconds to run
 sleep 10
@@ -11,3 +11,13 @@ cat /tmp/booktrack/testlog.txt
 
 # get the coverage
 tail -1 /tmp/booktrack/report.txt | awk '{print $NF}'
+
+
+coverage=$(tail -1 /tmp/booktrack/report.txt 2>/dev/null | awk '{print $NF}' | tr -d '%')
+echo "Total coverage: ${coverage:-Unavailable}%"
+
+# Compare as a number
+if [ -n "$coverage" ] && [ "$(echo "$coverage < 60" | bc)" -eq 1 ]; then
+  echo "Coverage $coverage% is too low!"
+  exit 1
+fi
